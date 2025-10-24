baa69733859c831b663404825a21dd29932c9553
Turtolo <Lauvstadnils4@gmail.com>
2025-10-25 00:27:56 +0200

    Fixed the load position, the raycast is wonky

--- Util/EntityLoader.cs
+++ Util/EntityLoader.cs
@@ -15,15 +15,13 @@ namespace ConstructEngine.Util;
 
 public class EntityLoader
 {
-
-    public static Dictionary<Entity, Vector2> EntityDict = new();
-
-    public static void GetEntityData(string filename)
+    public static Dictionary<Entity, Vector2> GetEntityData(string filename)
     {
         string json = File.ReadAllText(filename);
         var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
         OgmoReader.Root root = JsonSerializer.Deserialize<OgmoReader.Root>(json, options);
 
+        Dictionary<Entity, Vector2> EntityDict = new();
 
         foreach (var layer in root.layers)
         {
@@ -44,8 +42,6 @@ public class EntityLoader
 
                     Entity EntityInstance = (Entity)Activator.CreateInstance(type);
 
-                    Console.WriteLine(EntityInstance.GetType());
-
                     Vector2 EntityPosition = new Vector2(entity.x, entity.y);
 
                     EntityDict.Add(EntityInstance, EntityPosition);
@@ -53,88 +49,10 @@ public class EntityLoader
                 }
             }
         }
-    }
-    
-    public static List<Entity> LoadEntities(ContentManager content, string filename)
-    {
-        List <Entity> entities = new List<Entity>();
-        string json = File.ReadAllText(filename);
-        var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
-        OgmoReader.Root root = JsonSerializer.Deserialize<OgmoReader.Root>(json, options);
 
-        foreach (var layer in root.layers)
-        {
-            if (layer.entities == null)
-                continue;
-
-            foreach (var entity in layer.entities)
-            {
-
-                
-
-                string className = entity.name;
-                    
-                    
-                Assembly assembly = AppDomain.CurrentDomain.GetAssemblies()
-                    .FirstOrDefault(a => a.GetTypes().Any(t => t.Name == className && typeof(Entity).IsAssignableFrom(t)));
-
-
-                if (assembly != null)
-                {
-
-
-                    Type type = assembly.GetTypes()
-                        .First(t => t.Name == className && typeof(Entity).IsAssignableFrom(t));
-
-
-
-                    Entity instance = (Entity)Activator.CreateInstance(type);
-                    
-
-                    entities.Add(instance);
-                }
-            }
-        }
-        
-        return entities;
+        return EntityDict;
     }
     
     
-    public static List<Vector2> GetEntityPosition(ContentManager content, string filename)
-    {
-        List<Vector2> positionList = new List<Vector2>();
-        
-        string json = File.ReadAllText(filename);
-        var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
-        OgmoReader.Root root = JsonSerializer.Deserialize<OgmoReader.Root>(json, options);
-
-        foreach (var layer in root.layers)
-        {
-            if (layer.entities == null)
-            {
-                continue;
-            }
-
-            foreach (var entity in layer.entities)
-            {
-                
-                string className = entity.name;
-                
-                Assembly assembly = Assembly.GetExecutingAssembly();
-
-                
-                Type type = assembly.GetTypes().FirstOrDefault(t => t.Name == className);
-
-                if (type == null)
-                {
-                    positionList.Add(new Vector2(entity.x, entity.y));
-                    
-                }
-            }
-        }
-        
-        
-        return positionList;
-    }
 
 }
--- Util/Scene.cs
+++ Util/Scene.cs
@@ -15,6 +15,27 @@ public class Scene
 {
     public bool SceneFrozen { get; set; }
 
+
+
+    public static void InstantiateEntities(string filePath)
+    {
+        Dictionary<Entity, Vector2> EntityDictionary = EntityLoader.GetEntityData(filePath);
+
+        foreach (var entry in EntityDictionary)
+        {
+            Vector2 EntityLoadPosition = entry.Value;
+
+            Entity EntryEntity = entry.Key;
+
+            Entity.EntityList.Add(EntryEntity);
+
+            EntryEntity.Load();
+
+            EntryEntity.KinematicBase.Collider.Rect.X = (int)EntityLoadPosition.X;
+            EntryEntity.KinematicBase.Collider.Rect.Y = (int)EntityLoadPosition.Y;
+        }
+    }
+    
     public static void UpdateEntities(GameTime gameTime)
     {
         for (int i = Entity.EntityList.Count - 1; i >= 0; i--)
@@ -23,72 +44,7 @@ public class Scene
             e.Update(gameTime);
         }
     }
-
-    public static void InstantiateEntities(string filePath)
-    {
-
-
-        foreach(var entry in EntityLoader.EntityDict)
-        {
-            Vector2 EntityLoadPosition = entry.Value;
-
-            Entity EntryEntity = entry.Key;
-
-
-
-            Entity newEntity = (Entity)Activator.CreateInstance(EntryEntity.GetType());
-
-            Entity.EntityList.Add(newEntity);
-
-            newEntity.KinematicBase.Collider.Rect.X = (int)EntityLoadPosition.X;
-            newEntity.KinematicBase.Collider.Rect.Y = (int)EntityLoadPosition.Y;
-
-            newEntity.Load();
-
-        
-
-        }
-    }
     
-    public static void InstantiateEntities(ContentManager contentManager, string filePath)
-    {
-        Vector2 LoadPosition = new();
-        
-        List<Entity> loadedEntities = EntityLoader.LoadEntities(contentManager, filePath);
-        List<Vector2> positions = EntityLoader.GetEntityPosition(contentManager, filePath);
-
-        for (int i = 0; i < loadedEntities.Count; i++)
-        {
-            Type type = loadedEntities[i].GetType();
-            Entity newEntity = (Entity)Activator.CreateInstance(type);
-
-            if (i < positions.Count)
-            {
-                LoadPosition.X = (int)positions[i].X;
-                LoadPosition.Y = (int)positions[i].Y;
-
-            }
-
-
-            Entity.EntityList.Add(newEntity);
-        }
-
-        foreach (Entity.IEntity e in Entity.EntityList )
-        {
-            e.Load();
-
-        }
-        
-        foreach (Entity e in Entity.EntityList)
-        {
-            if (e.KinematicBase.Collider.HasRect)
-            {
-                e.KinematicBase.Collider.Rect.X = (int)LoadPosition.X;
-                e.KinematicBase.Collider.Rect.Y = (int)LoadPosition.Y;
-            }
-            
-        }
-    }
 
     public static void DrawEntities(SpriteBatch spriteBatch)
     {
